6162153be8fab4b35d9f6b9eaef960a0e5e19f6c
ecoen66 <ecoen@yahoo.com>
2016-04-19 22:05:57 -0700

    Clean up
    Edits for code cleanup, comments and light error handling.

--- Spark-add-members.py
+++ Spark-add-members.py
@@ -11,48 +11,49 @@ fname = "/Users/ecoen/Desktop/ecoen.txt"
 # This is the name of the Room that we want to add Members to
 the_room = "Test Room"
 
-# All of our REST calls will use the API url for Cisco Spark as the base URL
+# All of our REST calls will use the API URL for Cisco Spark as the base URL
 # So lets define a variable for the Spark URL so we don't have to keep typing it
-rooms_url = "https://api.ciscospark.com/"
+spark_url = "https://api.ciscospark.com/"
 
 # This is your personal Spark API Access Token from your profile at:
 # https://developer.ciscospark.com/?utm_source=Llab1&utm_medium=step2&utm_campaign=spark#
 auth_token = "Bearer " + "this-would-be-your-really-long-access-token"
 
 # These are the headers used for GET and POST for Spark
-headers = {"Authorization": auth_token}
-headers_put = {"Authorization": auth_token,'content-type': 'application/json'}
+headers_get = {"Authorization": auth_token}
+headers_post = {"Authorization": auth_token,"content-type": "application/json"}
 
-# Get Rooms
-# This function allows you to view a list of all the rooms that you belong to
-get_rooms_url = rooms_url + 'v1/rooms'
-post_members_url = rooms_url + 'v1/memberships'
+# Thes URLs will be used later to GET the rooms list and to POST new memberships
+get_spark_url = spark_url + "v1/rooms"
+post_members_url = spark_url + "v1/memberships"
 
-# Perform GET on get_rooms_url and load response into a json object
-get_rooms_response = requests.get(get_rooms_url, headers=headers)
+# Perform GET on get_spark_url and load response into a json object
+get_rooms_response = requests.get(get_spark_url, headers=headers_get)
 get_rooms_json = get_rooms_response.json()
 
-# Now let's read and display some specific information from the json
+# Now let's read some specific information from the json
 
-# set our parent as the top level response object
+# Set our parent as the top level response object
 rooms_parent = get_rooms_json["items"]
 
-# print("Rooms = ")
-
-# for each room returned, print the roomId, and look for a specific Room
+# Look through each room returned for our specific Room and return the_room_id
 for item in rooms_parent:
-#    print(item["title"], item["id"])
     if item["title"] == the_room:
         the_room_id = item["id"]
+        break
+else:
+    raise ValueError("The room was not found!")
 
-with open(fname) as f:
+# Read our text file of email addresses into an array 
+with open(fname, "r") as f:
     content = f.readlines()
 
+# POST a Member Add to the_room_id in Spark for each email address in the array
 for line in content:
     post_data = {"roomId": the_room_id,
                "personEmail": line[:len(line)-1],
                "isModerator": False
                }
-    put_member_response = requests.post(post_members_url, data=json.dumps(post_data), headers=headers_put)
-    print (line[:len(line)-1], put_member_response)
+    post_member_response = requests.post(post_members_url, data=json.dumps(post_data), headers=headers_post)
+    print (line[:len(line)-1], post_member_response)
 
